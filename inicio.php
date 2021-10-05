636a62ac7ad1651155998410f7329beb2f83c2ab
Pollitodetroya <ignaciogovo@gmail.com>
2021-10-05 13:51:29 +0200

    idea

---
+++ inicio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Foro 2º ASIR</h1>
+    <?php
+        echo "hello world"; 
+        $nombre="Horacio";
+        $x=3;
+        $y=4;
+        echo "</br> <p> HOLA </p>";
+        echo "$nombre tomate" . $x*$y . "copas";
+    ?>
+</body>
+</html>
